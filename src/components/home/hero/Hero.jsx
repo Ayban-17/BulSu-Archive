6d058ca2e13dd301d15b2d8af9263043d9032f1a
Ayban <mrkvnhrnndz17@gmail.com>
2023-06-05 00:15:06 +0800

    without react-reveal

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -1,5 +1,5 @@
 import gate from "../../../assets/gate.png";
-import Fade from "react-reveal/Fade";
+// import Fade from "react-reveal/Fade";
 import Title from "../reusable/Title";
 
 const styles = "text-xl font-bold text-red  md:text-3xl lg:text-4xl";
@@ -10,26 +10,26 @@ const Hero = () => {
   // };
   return (
     <div className="relative">
-      <Fade bottom distance="3em" duration={2000}>
-        <img className="h-64 min-w-bgImg sm:h-auto " src={gate} alt="gate" />
+      {/* <Fade bottom distance="3em" duration={2000}> */}
+      <img className="h-64 min-w-bgImg sm:h-auto " src={gate} alt="gate" />
 
-        <div className=" absolute left-16 top-7 flex flex-col  justify-center gap-5 p-4 w-72 bg-white rounded-xl sm:top-7 md:w-search lg:w-search lg:h-64 lg:left-40 lg:top-20 lg:px-10 lg:gap-10 xl:left-60 xl:top-40">
-          <div className="flex flex-col gap-4">
-            <Title title="Your Online " style={styles} />
-            <Title title="Literature Library" style={styles} />
-          </div>
-          <form className="bg-bgSearch flex justify-between ">
-            <input
-              type="text"
-              className="bg-bgSearch text-xs px-2 py-2 outline-none flex-1 md:p-3 lg:py-3 lg:px-5"
-              placeholder="What are you looking for?"
-            />
-            <button className="bg-red font-poppins text-white text-xs p-2 rounded-lg m-1 md:p-3 lg:py-3 lg:px-5">
-              Search
-            </button>
-          </form>
+      <div className=" absolute left-16 top-7 flex flex-col  justify-center gap-5 p-4 w-72 bg-white rounded-xl sm:top-7 md:w-search lg:w-search lg:h-64 lg:left-40 lg:top-20 lg:px-10 lg:gap-10 xl:left-60 xl:top-40">
+        <div className="flex flex-col gap-4">
+          <Title title="Your Online " style={styles} />
+          <Title title="Literature Library" style={styles} />
         </div>
-      </Fade>
+        <form className="bg-bgSearch flex justify-between ">
+          <input
+            type="text"
+            className="bg-bgSearch text-xs px-2 py-2 outline-none flex-1 md:p-3 lg:py-3 lg:px-5"
+            placeholder="What are you looking for?"
+          />
+          <button className="bg-red font-poppins text-white text-xs p-2 rounded-lg m-1 md:p-3 lg:py-3 lg:px-5">
+            Search
+          </button>
+        </form>
+      </div>
+      {/* </Fade> */}
     </div>
   );
 };
--- src/components/home/holdings/Introduction.jsx
+++ src/components/home/holdings/Introduction.jsx
@@ -1,5 +1,5 @@
 import { faCube } from "@fortawesome/free-solid-svg-icons";
-import { Fade } from "react-reveal";
+// import { Fade } from "react-reveal";
 import Title from "../reusable/Title";
 import Description from "../reusable/Description";
 import LearnMore from "../reusable/LearnMore";
@@ -13,22 +13,22 @@ const description = " text-center text-lg px-8 opacity-50 xl:text-left xl:px-0";
 const Introduction = () => {
   return (
     <div>
-      <Fade bottom distance="3em" duration={2000}>
-        <div className="flex flex-col items-center gap-5 px-6 ">
-          <Icon
-            icon={faCube}
-            style="text-5xl rounded-lg p-2 text-red bg-bgIcon  "
-          />
+      {/* <Fade bottom distance="3em" duration={2000}> */}
+      <div className="flex flex-col items-center gap-5 px-6 ">
+        <Icon
+          icon={faCube}
+          style="text-5xl rounded-lg p-2 text-red bg-bgIcon  "
+        />
 
-          <Title title="What is BulSU Archive ?" style={title} />
+        <Title title="What is BulSU Archive ?" style={title} />
 
-          <Description
-            description="The BulSU Archive preserves and disseminates a variety of academic works, including research papers and theses written by Bulacan State University—Sarmiento Campus students. "
-            style={description}
-          />
-          <LearnMore name="Learn More" style={learnMore} />
-        </div>
-      </Fade>
+        <Description
+          description="The BulSU Archive preserves and disseminates a variety of academic works, including research papers and theses written by Bulacan State University—Sarmiento Campus students. "
+          style={description}
+        />
+        <LearnMore name="Learn More" style={learnMore} />
+      </div>
+      {/* </Fade> */}
     </div>
   );
 };
